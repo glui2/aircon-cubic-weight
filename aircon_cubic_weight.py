7f15586aeb413533a32620a1529cba3d9992438a
Gavin <gavin.lui@gmail.com>
2020-11-03 14:09:12 +1100

    calculate average cubic weight from aircon objects

--- aircon_cubic_weight.py
+++ aircon_cubic_weight.py
@@ -5,6 +5,20 @@ print(f'Sending GET request to API...')
 kogan_url = f'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com'
 
 
+def calculate_cubic_weight(product):
+    # Cubic weight is calculated by multiplying the length, height and width of the parcel (metres).
+    product_dimensions = product['size']
+    length = product_dimensions['length']/100
+    width = product_dimensions['width']/100
+    height = product_dimensions['height']/100
+
+    # The result is then multiplied by the industry standard cubic weight conversion factor of 250.
+    conv_factor = 250
+
+    cubic_weight = length*width*height*conv_factor
+    return cubic_weight
+
+
 def get_aircon_objects(api_stem):
     api = kogan_url + api_stem
     response = requests.get(api)
@@ -18,14 +32,25 @@ def get_aircon_objects(api_stem):
 
     else:
         # base case: start returning aircon objects once there are no more items to list
-        print("end of list!")
+        print(f"end of list!")
 
     current_aircon_objects = [product_object for product_object in data['objects']
                               if product_object['category'] == 'Air Conditioners']
 
     all_aircon_objects = current_aircon_objects + aircon_objects
-    print(all_aircon_objects)
     return all_aircon_objects
 
 
-get_aircon_objects(f'/api/products/1')
+aircon_object_list = get_aircon_objects(f'/api/products/1')
+print(
+    f"List of all objects in the Air-Conditioner category: {aircon_object_list} \n")
+
+cubic_weights = [calculate_cubic_weight(
+    aircon_object) for aircon_object in aircon_object_list]
+print(
+    f"Cubic weight of each object in the Air-Conditioner category:\n {cubic_weights} \n")
+
+
+average_cubic_weight = sum(cubic_weights)/len(cubic_weights)
+print(
+    f"Average cubic weight of products in the Air-Conditioner category is: {average_cubic_weight}")
